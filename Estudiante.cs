a889dab16bddb0729b2783363ed317779f204bdc
Alexkcq <qalexander785@gmail.com>
2024-11-14 08:52:14 -0500

    Primer commit

---
+++ Estudiante.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examenT3
+{
+    public class Estudiante
+    {
+        public int Codigo { get; set; }
+        public int Nombre { get; set; }
+        public int Universidad { get; set; }
+        public Estudiante(int code, string nomb, string universidad)
+        { 
+            Codigo = code;
+            Nombre = nomb;
+            Universidad = universidad;
+        }
+        public over
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿string Opcion;
+
+void MenudeAlternativas()
+{
+    Console.WriteLine("1.****Crear****\n");
+    Console.WriteLine("2.****Listar****\n");
+    Console.WriteLine("3.****Modificar****\n");
+    Console.WriteLine("4.****Fin****\n");
+    Console.WriteLine("Seleccione una alternativa: ");
+}
+bool ContinuarProm = true;
+while (ContinuarProm)
+{
+    MenudeAlternativas();
+    Opcion = Console.ReadLine();
+    switch (Opcion)
+    {
+        case "a":
+            Console.WriteLine("\nIngrese"
+    }
+}
